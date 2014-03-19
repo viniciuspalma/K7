88475f9c9c466c73c5af78e06b0ab932d520f630
Vinicius Palma <vini_palma@outlook.com>
2014-03-19 00:01:07 -0300

    start development

--- src/js/main.js
+++ src/js/main.js
@@ -1,95 +1,56 @@
-(function(){
-  var btnPlay = document.querySelector('.button.play'),
-    btnPause = document.querySelector('.button.pause'),
-    btnStop = document.querySelector('.button.stop'),
-    btnPrev = document.querySelector('.button.prev'),
-    btnNext = document.querySelector('.button.next'),
-    roda1 = document.querySelector('#roda1'),
-    roda2 = document.querySelector('#roda2'),
-    audio = document.querySelector('#audio'),
-    title = document.querySelector('.up-title h5'),
-    src = document.querySelector('#audio1'),
-    imgFundo = document.querySelector('.imgFundo'),
-    imgSrc = document.querySelector('#first-img');
-    volMore = document.querySelector('.button.more'),
-    volLess = document.querySelector('.button.less'),
-    indexMusic = 0,
-    musics = [],
-    nomes = [],
-    url = window.location,
-    musicas = '{"musicas":[' +
-          '{"file":"elpaso.mp3","nome":"El Paso - Felina" },' +
-          '{"file":"wake.mp3","nome":"Wake me up - avicci" },' +
-            '{"file":"rapgod.mp3","nome":"Eminem - Rap God (Explicit)" }]}',
+var K7 = {
 
-    objMusic = eval("(" + musicas + ")");
+  var player = document.createElement('audio');
+    , playing = false
+    , playlist = []
+    , indexTrack = 0
+    , currentTrack;
 
-    console.log(objMusic.musicas.length);
+  this.animation = function() {
 
-  function playMusic(music){
-    music.play();
-    roda1.classList.add('def-rod-anima');
-    roda2.classList.add('def-rod-anima');
-    imgFundo.classList.add('imgAni');
-    title.innerHTML = src.getAttribute('alt');
-  }
-  function pauseMusic(music){
-    roda1.classList.remove('def-rod-anima');
-    roda2.classList.remove('def-rod-anima');
-    imgFundo.classList.remove('imgAni');
-    music.pause();
-    title.innerHTML += " Pause"
-  }
-  function stopMusic(music){
-    roda1.classList.remove('def-rod-anima');
-    roda2.classList.remove('def-rod-anima');
-    imgFundo.classList.remove('imgAni');
-    music.load();
-    title.innerHTML = "Click em Play para retomar";
-  }
-  function nextMusic(music){
-    if (indexMusic + 1 < objMusic.musicas.length) {indexMusic++;}
-    else{indexMusic = 0};
+  };
 
-    src.setAttribute('src', url + "/musicas/" + objMusic.musicas[indexMusic].file);
-    src.setAttribute('alt', objMusic.musicas[indexMusic].nome)
-    music.load();
-    playMusic(music);
-  }
-  function prevMusic(music){
-    if (indexMusic > 0) {indexMusic--;}
-    else{indexMusic = objMusic.musicas.length - 1};
+  this.playPause = function() {
+    if ( playing ) {
+      player.pause();
+      playing = false;
+      this.animation();
+    }
+    else {
+      player.play();
+      playing = true;
+      this.animation();
+    }
+  };
 
-    src.setAttribute('src', url + "/musicas/" + objMusic.musicas[indexMusic].file);
-    src.setAttribute('alt', objMusic.musicas[indexMusic].nome)
-    audio.load();
-    playMusic(music);
-  }
-  btnPlay.addEventListener('click', function(e) {
-    playMusic(audio);
-  });
+  this.stop = function() {
+    if ( playing ) {
+      player.stop();
+      playing = false;
+    }
+  };
 
-  btnPause.addEventListener('click', function(e) {
-    pauseMusic(audio);
-  });
+  this.setTrack = function() {
+    currentTrack = playlist.[this.indexTrack];
+    player.src = currentTrack.src;
+  };
 
-  btnStop.addEventListener('click', function(e) {
-    stopMusic(audio);
-  });
+  this.prev = function() {
+    indexTrack --;
 
-  btnNext.addEventListener('click', function(e) {
-    nextMusic(audio);
-  });
+    if (this.indexTrack < 0) this.indexTrack = 0;
 
-  btnPrev.addEventListener('click', function(e) {
-    prevMusic(audio);
-  });
+    this.setTrack();
+    this.play();
+  };
 
-  volLess.addEventListener('click', function(e) {
-    if(audio.volume >= 0.1){audio.volume -= 0.1;};
-  });
+  this.next = function() {
+    indexTrack ++;
 
-  volMore.addEventListener('click', function(e) {
-    if(audio.volume <= 0.9){audio.volume += 0.1;};
-  });
-})();
+    if (this.indexTrack > playlist.length) this.indexTrack = 0;
+
+    this.setTrack();
+    this.play();
+  };
+
+};
